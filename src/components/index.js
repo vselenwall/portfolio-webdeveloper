803e363130063a1b18de0be027aca09058c81b9c
vselenwall <veronica@selenwall.se>
2023-06-16 23:04:47 +0200

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-// export { default as Projects } from './projects/Projects';
-
-export { default as Gallery } from './gallery/PortfolioPhoto';
-export { default as Projects } from './gallery/PortfolioWeb';
-export { default as Navbar } from './navbar/Navbar';
